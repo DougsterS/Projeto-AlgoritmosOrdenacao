6fe26538c78b516ebb5ad2482264fc857ff5b831
Douglas Antonio Monteiro Alcantara Justino <33661367+DougsterS@users.noreply.github.com>
2018-10-27 12:57:09 -0300

    1.1
    Correção do MergeSort

--- algoritmosdeordenacao/MergeSort.java
+++ algoritmosdeordenacao/MergeSort.java
@@ -2,39 +2,50 @@ package algoritmosdeordenacao;
 
 public class MergeSort {
 	
-	public void organizar(int vetor[], int inicio, int fim) {
-		if(inicio < fim) {
-			int aux = (inicio+fim)/2;
-			
-			organizar(vetor,inicio,aux);
-			organizar(vetor,aux+1,fim);
-			intercalar(vetor,inicio,aux,fim);
-		}	
-	}
-	
-	public void intercalar(int vetor[],int inicio, int aux, int fim) {
-		
-		int vetorzinho[] = new int[vetor.length];
-		
-		for(int j = inicio; j<= aux; j++) {
-			vetorzinho[j] = vetor[j];
-		}
-		
-		for(int y = aux+1; y<fim;y++) {
-			vetorzinho[fim+aux-y] = vetor[y];
-		}
-		int x = inicio;
-		int z = fim-1;
-		
-		for(int seila = inicio; seila<fim; seila++) {
-			if(vetorzinho[x] <= vetorzinho[z]) {
-				vetor[seila] = vetorzinho[x];
-				x = x+1;
-			} else {
-				vetor[seila] = vetorzinho[z];
-				z = z-1;
-			}
-		}
-	}
+	public static int[] organizar(int [] array){
+
+        int size = array.length;
+        if(size<2)
+            return array;
+        int[] left = new int[(size/2)];
+        int[] right = new int[(size-(size/2))];
+        for(int i=0; i<(size/2); i++ ){
+            left[i] = array[i];
+        }
+        for(int i=(size/2); i<(size); i++ ){
+            right[i-(size/2)] = array[i];
+        }
+        organizar(left);
+        organizar(right);
+        int[] a= mergeSort(left, right, array);
+        return a;
+
+    }
+
+    public static int[] mergeSort(int[] left, int[] right,int[] array){
+        int i=0,j=0,k=0;
+        while(i<left.length && j<right.length){
+            if(left[i]<=right[j]){
+                array[k] = left[i];
+                i++;
+            }
+            else{
+                array[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<left.length){
+            array[k] = left[i];
+            i++;
+            k++;
+        }
+        while(j<right.length){
+            array[k] = right[j];
+            j++;
+            k++;
+        }
+        return array;
+    }        
 
 }
